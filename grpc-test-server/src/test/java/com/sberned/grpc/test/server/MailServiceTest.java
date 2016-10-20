2578a5d030b529bd1b908c929ccce32ea1f4b95f
Dmitry Chuiko <dchuiko@gmail.com>
2016-10-20 17:18:35 +0300

    fix tests

--- grpc-test-server/src/test/java/com/sberned/grpc/test/server/MailServiceTest.java
+++ grpc-test-server/src/test/java/com/sberned/grpc/test/server/MailServiceTest.java
@@ -23,7 +23,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = {Server.class})
+@SpringBootTest(classes = {ServerApp.class})
 public class MailServiceTest {
     private ManagedChannel channel;
 
